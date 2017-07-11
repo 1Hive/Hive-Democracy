74a4e2dc9ef8b2ec3fe6c43350814e4fb51c176d
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2017-07-10 23:24:37 -0300

    simplified storage

--- contracts/DelegativeDemocracy.sol
+++ contracts/DelegativeDemocracy.sol
@@ -12,10 +12,9 @@ contract DelegativeDemocracy {
     event Delegate(address who, address to);
     
     struct Delegation {
-        address to; //who recieved the votes
-        uint256 toIndex; //the position in the mapping of reciever
-        mapping (uint256 => address) fromIndex; //indexes of delegators
-        uint256 fromLength; //total delegators
+        address to; //who recieved the delegaton
+        uint256 toIndex; //index in from array of `to`
+        address[] from; //who is delegation
     }
     
     //returns the destination of an account's votes
@@ -40,23 +39,25 @@ contract DelegativeDemocracy {
         address _from = msg.sender;
         require(delegationOf(_to) != _from); //block impossible circular delegation
         Delegate(_from,_to);
-        address _oldTo = delegations[_from].to; //the delegation to be undone
+
+        //Removes from delegation 'receiver' the address listed there.
+        Delegation storage from = delegations[_from];
+        address _oldTo = from.to; //the delegation to be undone
         if(_oldTo != 0x0) { // _from was delegating?
             uint256 _oldToIndex = delegations[_from].toIndex; //msg.sender index in Delegator from list.
-            delegations[_oldTo].fromLength--; //decrement _oldTo from index size
-            if(_oldToIndex < delegations[_oldTo].fromLength)
-                delegations[_oldTo].fromIndex[_oldToIndex] = delegations[_oldTo].fromIndex[delegations[_oldTo].fromLength]; //put latest index in place of msg.sender position
-            delete delegations[_oldTo].fromIndex[delegations[_oldTo].fromLength]; //clear impossible position;
+            Delegation storage oldTo = delegations[_oldTo];
+            oldTo.from[_oldToIndex] = oldTo.from[oldTo.from.length - 1];
+            oldTo.from.length--;
         }
         
-        delegations[_from].to = _to; //register where our delegation is going
+        //Add the new delegation
+        from.to = _to; //register where our delegation is going
         if(_to != 0x0) { //_to is an account?
-            uint256 newPos = delegations[_to].fromLength;
-            delegations[_to].fromIndex[newPos] = _from; //add account into stack mapped to length
-            delegations[_from].toIndex = newPos; //register the index of our address delegation (for mapping clean)
-            delegations[_to].fromLength++; //increment _to from index size
+            Delegation storage to = delegations[_to];
+            from.toIndex = to.from.length; //register the index of our address delegation (for mapping clean)
+            to.from.push(_from);
         } else {
-            delegations[_from].toIndex = 0; //reset index
+            from.toIndex = 0; //reset index
         }
         
     }
@@ -65,9 +66,11 @@ contract DelegativeDemocracy {
     function _votesDelegatedTo(address _who, MiniMeToken _token, uint _block)
      internal
      constant returns(uint256 total) {
-        total = _token.balanceOfAt(_who,_block); // source of _who votes
-        for(uint256 i = 0; delegations[_who].fromLength > i;i++)  
-            total += _votesDelegatedTo(delegations[_who].fromIndex[i], _token, _block); //sum the from delegation votes
+        total = _token.balanceOfAt(_who, _block); // source of _who votes
+        address[] memory _from = delegations[_who].from;
+        uint _len = _from.length;
+        for(uint256 i = 0; _len > i; i++)  
+            total += _votesDelegatedTo(_from[i], _token, _block); //sum the from delegation votes
     }
 
 
